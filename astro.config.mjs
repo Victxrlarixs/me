64dc04fdd7d169a850e65ea091f1f367770e69c3
Victor Larios <35201083+Victxrlarixs@users.noreply.github.com>
2026-05-03 07:43:38 -0600

    fix: set base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://victxrlarixs.com',
+  site: 'https://victxrlarixs.github.io',
+  base: '/me',
   vite: {
     plugins: [tailwindcss()]
   },
